7b1336effbe8ea6a87171c9f255c53cc99ce746c
ImBard <nicota1234@gmail.com>
2022-05-22 21:07:31 -0300

    excluindo app controller/service/spect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
